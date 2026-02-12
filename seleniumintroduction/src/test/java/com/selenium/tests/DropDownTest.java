0d686857432cd535d446ae2d29337632a5565c9e
chandru <chandrucm000@gmail.com>
2026-02-12 13:23:59 +0530

    Reapply "Added"
    This reverts commit 622b4d4fda13927c005e5c769275da3cb74bbe84.

--- seleniumintroduction/src/test/java/com/selenium/tests/DropDownTest.java
+++ seleniumintroduction/src/test/java/com/selenium/tests/DropDownTest.java
@@ -1,8 +1,10 @@
 package com.selenium.tests;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import java.io.IOException;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -10,19 +12,14 @@ import org.testng.annotations.Test;
 
 import com.selenium.practice.BaseClass;
 import com.selenium.practice.DropDownPage;
-import com.selenium.practice.ScreenshotUtil;
+
+import utils.ScreenshotUtil;
 
 public class DropDownTest extends BaseClass {
 
 	DropDownPage dropdownpage;
 
-	@BeforeClass
-	public void setup()
-	{
-		initializeDriver();
-		dropdownpage = new DropDownPage(driver);
-		dropdownpage.getURL("https://the-internet.herokuapp.com/");
-	}
+	
 	
 	@Test
 	public void dropDown()
@@ -35,13 +32,6 @@ public class DropDownTest extends BaseClass {
 	public void screenshot() throws IOException
 	{
 		ScreenshotUtil.takeScreenshot(driver, "Drop_Down");
-	}
-	
-	
-	@AfterClass
-	public void quitbrowser()
-	{
-		quitDriver();
 	}
 	
 }
